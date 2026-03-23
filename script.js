ed997fb8f6e789f859897e14d382fca8a0f8ca6c
camily zdradek da silva <146744861+camilitinha@users.noreply.github.com>
2026-03-23 15:49:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+async function gerarPDF() {
+  const { jsPDF } = window.jspdf;
 
+  const doc = new jsPDF();
+
+  const exercicios = document.getElementById("exercicios").value;
+  const gabarito = document.getElementById("gabarito").value;
+
+  doc.setFontSize(12);
+
+  doc.text("Lista de Exercícios", 10, 10);
+  doc.text(exercicios, 10, 20);
+
+  doc.addPage();
+
+  doc.text("Gabarito", 10, 10);
+  doc.text(gabarito, 10, 20);
+
+  doc.save("lista.pdf");
+}
